8958a936a177cd33e99240a1f186d52393e51752
shubhamgrg04 <shubhamgrg04@gmail.com>
2015-02-15 18:13:07 +0530

    News Bytes
    News feed app which uses artificial intelligence (particularly NLP ) to
    shorten the content of news articles and extract the important content.

---
+++ DetailsPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using NewsBytes.Resources;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace NewsBytes
+{
+    public partial class DetailsPage : PhoneApplicationPage
+    {
+        // Constructor
+        public DetailsPage()
+        {
+            InitializeComponent();
+            textblock1.Text = "";
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+        }
+
+        // When page is navigated to set data context to selected item in list
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            string myurl = "";
+            textblock1.Text = "";
+            if (DataContext == null)
+            {
+                string selectedIndex = "";
+                if (NavigationContext.QueryString.TryGetValue("selectedItem", out selectedIndex))
+                {
+                    int index = int.Parse(selectedIndex);
+                }
+
+                myurl = NavigationContext.QueryString["itemUrl"];
+            }
+            string baseurl2 = "http://api.aylien.com/api/v1/summarize";
+            myurl = myurl.Replace("/", "%2F");
+            myurl = myurl.Replace(":", "%3A");
+            string finalurl = baseurl2 + "?url=" + myurl + "&mode=default&sentences_number=5";
+            //string finalurl = "http://api.aylien.com/api/v1/summarize?url=http%3A%2F%2Fwww.nasa.gov%2Fpress%2F2015%2Ffebruary%2Fmedia-invited-to-boeing-commercial-crew-access-tower-groundbreaking%2F&mode=default&sentences_number=5";
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Post, finalurl);
+            textblock1.Text = finalurl;
+
+            // Add our custom headers
+            requestMessage.Headers.Add("X-AYLIEN-TextAPI-Application-Key", "69cb0e7d3e052e470ae130922fd025dd");
+            requestMessage.Headers.Add("X-AYLIEN-TextAPI-Application-ID", "bfbb168d");
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.SendAsync(requestMessage);
+            string responseAsString = await response.Content.ReadAsStringAsync();
+            RootObject2 data2 = JsonConvert.DeserializeObject<RootObject2>(responseAsString);
+            if (data2.sentences == null) { textblock1.Text = "empty"; }
+            string total = "";
+            foreach (String sentence in data2.sentences)
+            {
+                total = total + sentence;
+            }
+            textblock1.Text = total;
+
+        }
+
+    }
+
+    public class RootObject2
+    {
+        public string text { get; set; }
+        public List<string> sentences { get; set; }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using NewsBytes.Resources;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace NewsBytes.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            this.Items = new ObservableCollection<ItemViewModel>();
+        }
+
+        /// <summary>
+        /// A collection for ItemViewModel objects.
+        /// </summary>
+        public ObservableCollection<ItemViewModel> Items { get; private set; }
+
+        private string _sampleProperty = "Sample Runtime Property Value";
+        /// <summary>
+        /// Sample ViewModel property; this property is used in the view to display its value using a Binding
+        /// </summary>
+        /// <returns></returns>
+        public string SampleProperty
+        {
+            get
+            {
+                return _sampleProperty;
+            }
+            set
+            {
+                if (value != _sampleProperty)
+                {
+                    _sampleProperty = value;
+                    NotifyPropertyChanged("SampleProperty");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sample property that returns a localized string
+        /// </summary>
+        public string LocalizedSampleProperty
+        {
+            get
+            {
+                return AppResources.SampleProperty;
+            }
+        }
+
+        public bool IsDataLoaded
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Creates and adds a few ItemViewModel objects into the Items collection.
+        /// </summary>
+        public async void LoadData()
+        {
+            HttpClient client = new HttpClient();
+            string baseurl = "http://content.guardianapis.com/search?q=debate&tag=politics/politics&from-date=2015-01-01&api-key=test";
+            string result = await client.GetStringAsync(baseurl);
+            
+            
+            
+            // Sample data; replace with real data
+           /* HttpClient client = new HttpClient();
+            string baseurl = "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/technology/7.json?api-key=sample-key";
+            string result = await client.GetStringAsync(baseurl);
+            //string myurl = "";
+            */
+            RootObject data = JsonConvert.DeserializeObject<RootObject>(result);
+
+            foreach (Result item in data.response.results)
+            {
+                int i = 0;
+                this.Items.Add(new ItemViewModel() { ID = i.ToString(), LineOne = item.webTitle, LineTwo = item.webPublicationDate, LineThree = item.webUrl });
+                i++;
+            }
+            this.IsDataLoaded = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        /*
+        public class Result
+        {
+            public string url { get; set; }
+            public string adx_keywords { get; set; }
+            public string column { get; set; }
+            public string section { get; set; }
+            public string byline { get; set; }
+            public string type { get; set; }
+            public string title { get; set; }
+            public string @abstract { get; set; }
+            public string published_date { get; set; }
+            public string source { get; set; }
+            public object id { get; set; }
+            public object asset_id { get; set; }
+            public int views { get; set; }
+            public List<string> des_facet { get; set; }
+            public List<string> org_facet { get; set; }
+            public object per_facet { get; set; }
+            public object geo_facet { get; set; }
+            public object media { get; set; }
+        }
+
+        public class RootObject
+        {
+            public string status { get; set; }
+            public string copyright { get; set; }
+            public int num_results { get; set; }
+            public List<Result> results { get; set; }
+        }
+        */
+
+
+        public class Result
+        {
+            public string webTitle { get; set; }
+            public string webPublicationDate { get; set; }
+            public string sectionId { get; set; }
+            public string id { get; set; }
+            public string webUrl { get; set; }
+            public string apiUrl { get; set; }
+            public string sectionName { get; set; }
+        }
+/// <summary>
+/// //////////////////////////////////////////////////////////////////////////////////////
+/// </summary>
+        public class Response
+        {
+            public string status { get; set; }
+            public string userTier { get; set; }
+            public int total { get; set; }
+            public int startIndex { get; set; }
+            public int pageSize { get; set; }
+            public int currentPage { get; set; }
+            public int pages { get; set; }
+            public string orderBy { get; set; }
+            public List<Result> results { get; set; }
+        }
+
+        public class RootObject
+        {
+            public Response response { get; set; }
+        }
+    }
+}
