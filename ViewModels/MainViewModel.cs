1fb7f99d0bc695ca6fa94d73abc97c9d1b174486
shubhamgrg04 <shubhamgrg04@gmail.com>
2015-02-20 23:40:07 +0530

    Changing section showing section name to show published date

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -81,7 +81,7 @@ namespace NewsBytes.ViewModels
             {
                 int i = 0;
                 if (item.sectionName == "Comment is free") item.sectionName = "Miscellaneous";
-                this.Items.Add(new ItemViewModel() { ID = i.ToString(), LineOne = item.webTitle, LineTwo = item.sectionName, LineThree = item.webUrl });
+                this.Items.Add(new ItemViewModel() { ID = i.ToString(), LineOne = item.webTitle, LineTwo = item.webPublicationDate, LineThree = item.webUrl });
                 i++;
             }
             this.IsDataLoaded = true;
